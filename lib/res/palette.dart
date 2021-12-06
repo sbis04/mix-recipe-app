cd46cdfdc73238bfff0df21c7075d8e7712b1f06
Souvik Biswas <sbis1999@gmail.com>
2021-12-06 09:21:00 +0530

    Add colors of the app

---
+++ lib/res/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const recipeBlack = Color(0xFF0D0D0D);
+  static const recipeWhite = Color(0xFFFFFFFF);
+  static const recipeGreen = Color(0xFF3EBD6A);
+  static const recipeGreenLight = Color(0xFFE8F5EF);
+}

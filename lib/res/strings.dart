8a42df4b33876853500ee5c23f887edaa12e1276
Souvik Biswas <sbis1999@gmail.com>
2021-12-06 12:42:54 +0530

    Define strings

---
+++ lib/res/strings.dart
@@ -0,0 +1,4 @@
+// Strings that are used throughout the app
+const String coverTitleString = 'Cooking Experience Like a Chef';
+const String coverSubtitleString =
+    'Let\'s make a delicious dish with the best recipe for the family';

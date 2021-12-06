35a9528a6ac369a2a401b88d220180d578d216d2
Souvik Biswas <sbis1999@gmail.com>
2021-12-06 15:08:06 +0530

    Update mix

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -1,4 +1,8 @@
 // Strings that are used throughout the app
-const String coverTitleString = 'Cooking Experience Like a Chef';
+const String coverTitleString = 'Cooking experience like a chef';
 const String coverSubtitleString =
     'Let\'s make a delicious dish with the best recipe for the family';
+
+const String findPageTitleString = 'Find best recipe\nFor cooking';
+const String findPageDishName = 'Salmon Sushi';
+const List<String> chipTexts = ['All', 'Sushi', 'Burger', 'Dessert'];

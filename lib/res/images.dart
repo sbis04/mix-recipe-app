f0b829840d53ee080abd9942534d14c428d61fb9
Souvik Biswas <sbis1999@gmail.com>
2021-12-08 22:51:15 +0530

    Add new image paths

--- lib/res/images.dart
+++ lib/res/images.dart
@@ -1,2 +1,5 @@
 // Image paths of the assets
 const String coverImagePath = 'assets/sushi-cover.jpeg';
+const String recipeImagePath1 = 'assets/sushi-1.jpg';
+const String recipeImagePath2 = 'assets/sushi-2.jpg';
+const String recipeImagePath3 = 'assets/sushi-3.jpg';

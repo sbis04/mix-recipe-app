357f0463b606d54e841df3723e35591cc1b991bc
Souvik Biswas <sbis1999@gmail.com>
2021-12-08 22:50:57 +0530

    Add new strings

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -3,6 +3,10 @@ const String coverTitleString = 'Cooking experience like a chef';
 const String coverSubtitleString =
     'Let\'s make a delicious dish with the best recipe for the family';
 
+// find page
 const String findPageTitleString = 'Find best recipe\nFor cooking';
 const String findPageDishName = 'Salmon Sushi';
 const List<String> chipTexts = ['All', 'Sushi', 'Burger', 'Dessert'];
+
+const String recipeTitle = "Salmon Sushi";
+const String recipeDescription = "12 Ingredients | 40 Min";
